d29d39620f7853383be0f8743ec7b881d743e5f2
Jose Martinez <josemartinez8282@gmail.com>
2020-02-19 22:03:52 -0800

    cp

--- susys/src/app/app.module.ts
+++ susys/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { MainComponent } from './views/main/main.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { MenuComponent } from './service/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
